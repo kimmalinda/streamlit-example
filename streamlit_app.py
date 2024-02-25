e95ff7371a894de94ac90e63489d7d6ff15f722e
kimmalinda <161007836+kimmalinda@users.noreply.github.com>
2024-02-26 02:01:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
+#Data
 import numpy as np
 import pandas as pd
-import streamlit as st
-st.write('111')
-tt = st.text_input('')
-st.write(tt)
+from sklearn.preprocessing import OneHotEncoder
+#Model
+from sklearn.metrics import classification_report, accuracy_score, make_scorer, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+import joblib
+

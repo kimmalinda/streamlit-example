af244dc47303c3a62935800bd1d6c8bff1544ead
kimmalinda <161007836+kimmalinda@users.noreply.github.com>
2024-03-08 14:56:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,21 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+
 import joblib
+#Data
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+#Model
+from sklearn.metrics import classification_report, accuracy_score, make_scorer, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 # Read original dataset
 iris_df = pd.read_csv('iris.csv')

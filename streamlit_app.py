d33bb3c027f11f975de051642d6507abb71d248b
kimmalinda <161007836+kimmalinda@users.noreply.github.com>
2024-03-08 14:45:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,29 +3,6 @@ import pandas as pd
 import numpy as np
 import joblib
 
-
-st.title('Classifying Iris Flowers')
-st.markdown('Toy model to play to classify iris flowers into setosa, versicolor, virginica')
-st.header('Plant Features')
-col1, col2 = st.columns(2)
-with col1:
-  st.text('Sepal characteristics')
-  sepal_l = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
-  sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
-with col2:
-  st.text('Pepal characteristics')
-  petal_l = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
-  petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
-
-r = st.button('Predict type of Iris')
-def predict(data):
-  clf = joblib.load('rf_model.sav')
-  return clf.predict(data)
-
-if r:
-  result = predict(np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
-  st.text(result[0])
-
 # Read original dataset
 iris_df = pd.read_csv('iris.csv')
 iris_df.sample(frac=1)
@@ -46,6 +23,29 @@ accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy}') #Accuracy: 0.91
 
 joblib.dump(clf, 'rf_model.sav')
+
+st.title('Classifying Iris Flowers')
+st.markdown('Toy model to play to classify iris flowers into setosa, versicolor, virginica')
+st.header('Plant Features')
+col1, col2 = st.columns(2)
+with col1:
+  st.text('Sepal characteristics')
+  sepal_l = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
+  sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
+with col2:
+  st.text('Pepal characteristics')
+  petal_l = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
+  petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
+
+r = st.button('Predict type of Iris')
+
+def predict(data):
+  clf = joblib.load('rf_model.sav')
+  return clf.predict(data)
+
+if r:
+  result = predict(np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
+  st.text(result[0])
 
 
 

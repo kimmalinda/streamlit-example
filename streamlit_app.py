6ee1701d12484c2b95c2f3d940ce49bc08a1b843
kimmalinda <161007836+kimmalinda@users.noreply.github.com>
2024-03-09 02:35:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ def cGPAX(a):
     b = 'Beware Class'
   return b
 
-def GradeToNum(a):
+def GradeToNum():
   if df1.Grade == "A":
     b = 4
   elif df1.Grade =="B+":
@@ -86,3 +86,109 @@ def GradeToNum(a):
   else:
     b = 0
   return b
+
+def CleanText(text0,major):
+  course=[]
+  credit=[]
+  grade=[]
+  text = text0.replace("\t", " ")
+  text = text.replace("\n"," ")
+  text = text.replace("/"," ")
+  text = text.split(" ")
+  text =list(filter(None, text))
+  num_of_row = len(text)
+  for i in range(num_of_row):
+    if text[i][0].isnumeric() and len(text[i]) == 6:
+      course.append(int(text[i]))
+      for j in range(i+1,i+10):
+        if text[j].isnumeric() and not text[j+1].isnumeric():
+          credit.append(int(text[j]))
+          grade.append(text[j+1])
+          break
+
+  df1 = pd.DataFrame()
+  df1 = pd.DataFrame(columns=['Course', 'Credit','Grade'])
+  df1['Course'] = course
+  df1['Credit'] = credit
+  df1['Grade'] = grade
+  GradeToNum()
+
+  match major:
+    case "Math":
+      mc = 252
+    case "Stat":
+      mc = 255
+    case "Chem":
+      mc = 256
+    case "Bio":
+      mc = 258
+    case "Phys":
+      mc = 261
+    case "App-Phys":
+      mc = 262
+    case "Com-Sci":
+      mc = 254
+    case "IT":
+      mc = 273
+  genEdgrade = 0
+  genEdcredit = 0
+  majorgrade = 0
+  majorcredit = 0
+  othergrade = 0
+  othercredit = 0
+
+
+  for i in range(len(df1)):
+    if df1['Course'][i] // 1000 == 1:
+      if df1['Course'][i] != 1281:
+        A = genEdcredit * genEdgrade
+        genEdcredit = genEdcredit + df1['Credit'][i]
+        genEdgrade = (A + df1['Credit'][i]*df1['Grade'][i])/genEdcredit
+      else:
+          continue
+    elif df1['Course'][i] // 1000 == mc:
+      B = majorcredit * majorgrade
+      majorcredit = majorcredit + df1['Credit'][i]
+      majorgrade = (B + df1['Credit'][i]*df1['Grade'][i])/majorcredit
+    else:
+      C = othercredit * othergrade
+      othercredit = othercredit + df1['Credit'][i]
+      othergrade = (C + df1['Credit'][i]*df1['Grade'][i])/othercredit
+  return [genEdgrade,majorgrade,othergrade]
+
+def SplitData(df,test_size):
+  total_index = list(df.index)
+  A =[]
+  B =[]
+  C=[]
+  D=[]
+  for i in total_index:
+    if df['ClassGPAX'][i] == 'A':
+      A.append(i)
+    elif df['ClassGPAX'][i] == 'B':
+      B.append(i)
+    elif df['ClassGPAX'][i] == 'C':
+      C.append(i)
+    else:
+      D.append(i)
+
+  nA = math.floor(test_size*(len(A)))
+  nB = math.floor(test_size*(len(B)))
+  nC = math.floor(test_size*(len(C)))
+  nD = math.floor(test_size*(len(D)))
+  random.shuffle(A)
+  random.shuffle(B)
+  random.shuffle(C)
+  random.shuffle(D)
+  test_index = A[:nA] + B[:nB] + C[:nC] + D[:nD]
+  train_index = list(set(total_index)-set(test_index))
+
+  #result = []
+  df_test = df.loc[test_index]
+  df_train = df.loc[train_index]
+  X_train = df_train.drop({'ClassGPAX'},axis = 1)
+  X_test = df_test.drop({'ClassGPAX'},axis = 1)
+  y_train = df_train['ClassGPAX']
+  y_test = df_test['ClassGPAX']
+
+  return [X_train,X_test,y_train,y_test]

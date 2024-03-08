8e3be096a18ddd0fe85fc5e3872b911e6272ac84
kimmalinda <161007836+kimmalinda@users.noreply.github.com>
2024-03-08 15:12:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 import joblib
 #Data
 import numpy as np
@@ -11,30 +10,251 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import accuracy_score
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
 
-# Read original dataset
-iris_df = pd.read_csv('iris.csv')
-iris_df.sample(frac=1)
-# selecting features and target data
-X = iris_df[['sepal.length', 'sepal.width', 'petal.length', 'petal.width']]
-y = iris_df[['variety']]
-# split data into train and test sets
-# 70% training and 30% test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y)
-# create an instance of the random forest classifier
-clf = RandomForestClassifier(n_estimators=100)
-# train the classifier on the training data
-clf.fit(X_train, y_train)
-# predict on the test set
-y_pred = clf.predict(X_test)
-# calculate accuracy
-accuracy = accuracy_score(y_test, y_pred)
-#print(f'Accuracy: {accuracy}') #Accuracy: 0.91
+
+def cExamPre(a):
+  match a:
+    case "ทบทวน อ่านหนังสือคนเดียว":
+      b = "w_friend"
+    case "ติวหนังสือกับกลุ่มเพื่อน":
+      b = "own"
+    case "ไม่อ่าน":
+      b = "No"
+  return b
+
+def oh(df):
+  df.ExamPrepare = df.ExamPrepare.apply(cExamPre)
+  ohe = OneHotEncoder(handle_unknown='ignore',sparse_output=False).set_output(transform='pandas')
+  ohetransform = ohe.fit_transform(df[['gender','ExamPrepare']])
+  df = pd.concat([df, ohetransform],axis =1)
+  return df
+
+#แทนค่าBehavior
+def Behavior(a):
+  match a:
+    case "ไม่ขาดเรียนเลย":
+      b = 3
+    case "ขาดเรียนบ้างเล็กน้อย (ขาดเรียนไม่เกิน 3 ครั้งของภาคเรียน)":
+      b = 2
+    case "ขาดเรียนระดับปานกลาง (ขาดเรียนเกิน 3 ครั้ง แต่ไม่ถึงครึ่งของภาคเรียน)":
+      b = 1
+    case "ขาดเรียนเป็นส่วนใหญ่ (ขาดเกินครึ่งของภาคเรียน)":
+      b = 0
+  return b
+
+def choice(a):
+  match a:
+    case "ชอบ" | "ทำ":
+      b = 1
+    case "ไม่ชอบ" | "ไม่ได้ทำ":
+      b = 0
+  return b
+
+
+#เคลียร์GPA
+def clearGPA(df):
+  for i in df.index:
+    if len(df['ClassGPAX'][i]) > 4:
+      df['ClassGPAX'][i] = df['ClassGPAX'][i][-4:]
+  return df
+
+#แปลงGPAเป็นรูปแบบของคลาส
+def c1GPAX(a):
+  grade  = float(a)
+  if 3.25 <= grade <= 4.00:
+    b = 'A'
+  elif 2.75 <= grade <= 3.24:
+    b = 'B'
+  elif 2.25 <= grade <= 2.74:
+    b = 'C'
+  else:
+    b = 'D'
+  return b
+
+def cGPAX(a):
+  grade  = float(a)
+  if 3.25 <= grade <= 4.00:
+    b = 'Honor Class'
+  elif 2.75 <= grade <= 3.24:
+    b = 'Medium Class'
+  elif 2.25 <= grade <= 2.74:
+    b = 'Lower Class'
+  else:
+    b = 'Beware Class'
+  return b
+
+
+#แปลงเกรดเป็นเลข
+def GradeToNum(a):
+  match a:
+    case "A":
+      b = 4
+    case "B+":
+      b = 3.5
+    case "B":
+      b = 3
+    case "C+":
+      b = 2.5
+    case "C":
+      b = 2
+    case "D+":
+      b = 1.5
+    case "D":
+      b = 1
+    case _:
+      b = 0
+  return b
+
+def cmajor(a):
+  match a:
+    case "คณิตศาสตร์":
+      b = "Math"
+    case "สถิติ":
+      b = "Stat"
+    case "เคมี":
+      b = "Chem"
+    case "ชีววิทยา":
+      b = "Bio"
+    case "ฟิสิกส์":
+      b = "Phys"
+    case "ฟิสิกส์ประยุกต์":
+      b = "App-Phys"
+    case "วิทยาการคอมพิวเตอร์":
+      b = "Com-Sci"
+    case "เทคโนโลยีสารสนเทศ":
+      b = "IT"
+  return b
+
+def CleanText(text0,major):
+  course=[]
+  credit=[]
+  grade=[]
+  text = text0.replace("\t", " ")
+  text = text.replace("\n"," ")
+  text = text.replace("/"," ")
+  text = text.split(" ")
+  text =list(filter(None, text))
+  num_of_row = len(text)
+  for i in range(num_of_row):
+    if text[i][0].isnumeric() and len(text[i]) == 6:
+      course.append(int(text[i]))
+      for j in range(i+1,i+10):
+        if text[j].isnumeric() and not text[j+1].isnumeric():
+          credit.append(int(text[j]))
+          grade.append(text[j+1])
+          break
+
+  df1 = pd.DataFrame()
+  df1 = pd.DataFrame(columns=['Course', 'Credit','Grade'])
+  df1['Course'] = course
+  df1['Credit'] = credit
+  df1['Grade'] = grade
+  df1.Grade = df1.Grade.apply(GradeToNum)
+
+  match major:
+    case "Math":
+      mc = 252
+    case "Stat":
+      mc = 255
+    case "Chem":
+      mc = 256
+    case "Bio":
+      mc = 258
+    case "Phys":
+      mc = 261
+    case "App-Phys":
+      mc = 262
+    case "Com-Sci":
+      mc = 254
+    case "IT":
+      mc = 273
+  genEdgrade = 0
+  genEdcredit = 0
+  majorgrade = 0
+  majorcredit = 0
+  othergrade = 0
+  othercredit = 0
+
+
+  for i in range(len(df1)):
+    if df1['Course'][i] // 1000 == 1:
+      if df1['Course'][i] != 1281:
+        A = genEdcredit * genEdgrade
+        genEdcredit = genEdcredit + df1['Credit'][i]
+        genEdgrade = (A + df1['Credit'][i]*df1['Grade'][i])/genEdcredit
+      else:
+          continue
+    elif df1['Course'][i] // 1000 == mc:
+      B = majorcredit * majorgrade
+      majorcredit = majorcredit + df1['Credit'][i]
+      majorgrade = (B + df1['Credit'][i]*df1['Grade'][i])/majorcredit
+    else:
+      C = othercredit * othergrade
+      othercredit = othercredit + df1['Credit'][i]
+      othergrade = (C + df1['Credit'][i]*df1['Grade'][i])/othercredit
+  return [genEdgrade,majorgrade,othergrade]
+
+def SplitData(df,test_size):
+  total_index = list(df.index)
+  A =[]
+  B =[]
+  C=[]
+  D=[]
+  for i in total_index:
+    if df['ClassGPAX'][i] == 'A':
+      A.append(i)
+    elif df['ClassGPAX'][i] == 'B':
+      B.append(i)
+    elif df['ClassGPAX'][i] == 'C':
+      C.append(i)
+    else:
+      D.append(i)
+
+  nA = math.floor(test_size*(len(A)))
+  nB = math.floor(test_size*(len(B)))
+  nC = math.floor(test_size*(len(C)))
+  nD = math.floor(test_size*(len(D)))
+  random.shuffle(A)
+  random.shuffle(B)
+  random.shuffle(C)
+  random.shuffle(D)
+  test_index = A[:nA] + B[:nB] + C[:nC] + D[:nD]
+  train_index = list(set(total_index)-set(test_index))
+
+  #result = []
+  df_test = df.loc[test_index]
+  df_train = df.loc[train_index]
+  X_train = df_train.drop({'ClassGPAX'},axis = 1)
+  X_test = df_test.drop({'ClassGPAX'},axis = 1)
+  y_train = df_train['ClassGPAX']
+  y_test = df_test['ClassGPAX']
+
+  return [X_train,X_test,y_train,y_test]
+
+df = pd.read_csv(r'/content/drive/MyDrive/Thesis/Data_csv/ver57math_BE.csv')
+#เพิ่มคอลัมเกรด
+df['GPAGenEd'] = None
+df['GPAMajor'] = None
+df['GPAOther'] = None
+
+df = oh(df)
+df.major = df.major.apply(cmajor)
+df.Good_math = df.Good_math.apply(choice)
+df.part_time= df.part_time.apply(choice)
+df.GenEdCA = df.GenEdCA.apply(Behavior)
+df.MajorCA = df.MajorCA.apply(Behavior)
+df.OtherCA = df.OtherCA.apply(Behavior)
+clearGPA(df)
+df.ClassGPAX = df.ClassGPAX.apply(cGPAX)
+for ind in df.index:
+  res = CleanText(df['gradeText'][ind],df['major'][ind])
+  df['GPAGenEd'][ind] = res[0]
+  df['GPAMajor'][ind] = res[1]
+  df['GPAOther'][ind] = res[2]
+df = df.drop(df.columns[[0,1,3,9]], axis=1)
+df.rename(columns = {'gender_ชาย':'male','gender_หญิง':'female'}, inplace = True)
+df
 
 joblib.dump(clf, 'rf_model.sav')
 

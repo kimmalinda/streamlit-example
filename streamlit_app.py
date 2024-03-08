927ac08c3b155a462dbd4fa51326e6933b259f17
kimmalinda <161007836+kimmalinda@users.noreply.github.com>
2024-03-08 14:18:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,4 @@
-# Read original dataset
-iris_df = pd.read_csv('/content/drive/MyDrive/Thesis/ข้อมูลcsv/iris.csv')
-iris_df.sample(frac=1)
-# selecting features and target data
-X = iris_df[['sepal.length', 'sepal.width', 'petal.length', 'petal.width']]
-y = iris_df[['variety']]
-# split data into train and test sets
-# 70% training and 30% test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y)
-# create an instance of the random forest classifier
-clf = RandomForestClassifier(n_estimators=100)
-# train the classifier on the training data
-clf.fit(X_train, y_train)
-# predict on the test set
-y_pred = clf.predict(X_test)
-# calculate accuracy
-accuracy = accuracy_score(y_test, y_pred)
-#print(f'Accuracy: {accuracy}') #Accuracy: 0.91
 
-joblib.dump(clf, 'rf_model.sav')
 
 st.title('Classifying Iris Flowers')
 st.markdown('Toy model to play to classify iris flowers into setosa, versicolor, virginica')

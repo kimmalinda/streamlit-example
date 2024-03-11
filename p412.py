0b90e2b88737478f0c856eb33b9778509bc2a6a2
kimmalinda <161007836+kimmalinda@users.noreply.github.com>
2024-03-11 18:39:16 +0700

    Update p412.py

--- p412.py
+++ p412.py
@@ -14,17 +14,16 @@ from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 
 #On Web
-st.title('การทำนายระดับผลการเรียน')
-st.markdown('กรุณากรอกข้อมูลให้ครบเพื่อใช้ในการทำนาย')
+st.title('การพยากรณ์ระดับผลการเรียนเฉลี่ตลอดหลักสูตร')
+st.markdown('กรุณากรอกข้อมูลให้ครบถ้วนเพื่อใช้ในการทำนาย')
 st.image("ขั้นตอน.png",caption='ขั้นตอนวิธีการกรอกผลการเรียนแต่รายวิชาในชั้นปีที่ 1')
-st.header('ผลการเรียนแต่ละรายวิชาชั้นปีที่ 1 ')
 col1, col2 = st.columns(2)
 with col1:
   st.text('สาขา')
   major1 = st.selectbox("สาขาที่เรียน",('คณิตศาสตร์','สถิติ','ฟิสิกส์','ฟิสิกส์ประยุกต์','เคมี','ชีววิทยา','วิทยาการคอมพิวเตอร์','เทคโนโลยีสารสนเทศ'))
 with col2:
   st.text('เกรดแต่ละรายวิชาชั้นปีที่ 1')
-  grade1 = st.text_input('copy ผลการเรียนชั้นปีที่ 1')
+  grade1 = st.text_input('ผลการเรียนแต่รายวิชาในชั้นปีที่ 1')
   
 re = st.button('Predict class of grade')
 

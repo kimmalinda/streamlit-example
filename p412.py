32395fe6e1479f86a5740d1a9c9ed860464ac1e2
kimmalinda <161007836+kimmalinda@users.noreply.github.com>
2024-03-11 15:03:44 +0700

    Update p412.py

--- p412.py
+++ p412.py
@@ -5,6 +5,12 @@ import numpy as np
 import pandas as pd
 import math
 import random
+from sklearn.metrics import classification_report, accuracy_score, make_scorer, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 
 #On Web
 st.title('การทำนายระดับผลการเรียน')

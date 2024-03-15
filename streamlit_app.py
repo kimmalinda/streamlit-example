b24c878feb4a55850241d060564920ed5af1119a
kimmalinda <161007836+kimmalinda@users.noreply.github.com>
2024-03-16 00:37:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -312,7 +312,7 @@ for ind in df.index:
   df['GPAOther'][ind] = res[2]
 df = df.drop(df.columns[[0,2,7]], axis=1)
 df.rename(columns = {'gender_ชาย':'male','gender_หญิง':'female'}, inplace = True)
-arr = np.array([[df.part_time[0],df.Good_math[0],df.df.GenEdCA[0],df.MajorCA[0],df.OtherCA[0],df.GPAGenEd[0],df_pred.GPAMajor[0],
+arr = np.array([[df.part_time[0],df.Good_math[0],df.GenEdCA[0],df.MajorCA[0],df.OtherCA[0],df.GPAGenEd[0],df.GPAMajor[0],
                  df.GPAOther[0],df.male[0],df.female[0],df.ExamPre_No[0],df.ExamPre_own[0],df.ExamPre_w_friend[0]]])
 
 #result

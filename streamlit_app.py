76ab1d690eee0a5fb43e1dacfb2bac2da0126795
kimmalinda <161007836+kimmalinda@users.noreply.github.com>
2024-03-16 01:53:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -397,8 +397,8 @@ if re:
   result = predict(arr)
   st.text([result[0]])
   st.write("เกณฑ์การให้ระดับผลการเรียนเฉลี่ยตลอดหลักสูตรดังนี้")
-  col1,mid, col2 = st.columns([1,1,20])
+  col1,mid, col2 = st.columns([1,1,10])
   with col1:
-    st.image('มง.png', width=60)
+    st.image('มง.png', width=40)
   with col2:
     st.write('Honor Class หมายถึง ผลการเรียนเฉลี่ยตั้งแต่ 3.25 ขึ้นไป')
